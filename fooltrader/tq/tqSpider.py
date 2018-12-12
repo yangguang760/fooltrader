adbeb26ab78b1517e8f3946bc772cb14cb7776fc
Guang YANG <yangguang760@gmail.com>
2018-12-12 10:05:00 +0800

    add eg contract

--- fooltrader/tq/tqSpider.py
+++ fooltrader/tq/tqSpider.py
@@ -6,9 +6,18 @@ from contextlib import closing
 import os
 from fooltrader.tq import tqdownloader
 from tqsdk.api import TqApi
-
+import logging
+logging.basicConfig(level=logging.DEBUG,#控制台打印的日志级别
+                    filename='yg.log',
+                    filemode='a',##模式，有w和a，w就是写模式，每次都会重新写日志，覆盖之前的日志
+                    #a是追加模式，默认如果不写的话，就是追加模式
+                    format=
+                    '%(asctime)s - %(pathname)s[line:%(lineno)d] - %(levelname)s: %(message)s'
+                    #日志格式
+                    )
 def scrawl_tick():
     agg = agg_future_dayk()
+    logging.info("start filter existed symbols")
     trading_dates = get_trading_calendar(security_type="future",exchange="shfe")
     tdates = {}
     for i in range(len(trading_dates)):
@@ -16,9 +25,12 @@ def scrawl_tick():
             tdates[datetime.strptime(trading_dates[i],'%Y%m%d')]=datetime.strptime(trading_dates[i-1],'%Y%m%d')
     path = "/home/yang/D/mdata/tqtick"
     filteredTradingDates = list(filter(lambda y:y>datetime(2018,11,30,0,0), map(lambda x:datetime.strptime(x,'%Y%m%d'),trading_dates)))
+    logging.info("complete filter existed symbols")
     exchanges = ["shfe","cffex","dce","czce"]
+    logging.info("start getting tick data")
     api = TqApi(account_id="SIM",url="ws://192.168.56.1:7777")
     for ex in exchanges:
+        logging.info(ex+": start getting tick")
         currentYearData = agg.getCurrentYearData(ex)
         currentYearData = currentYearData[currentYearData['date'].isin(filteredTradingDates)]
         pathpair=list(map(lambda x:(x[1].strftime('%Y%m%d')+"-"+x[0],x[0],x[1]) ,currentYearData[['symbol','date']].values))
@@ -38,3 +50,4 @@ def scrawl_tick():
                     api.wait_update()
                     # print("progress:  tick:%.2f%%" %  td.get_progress())
                 print("done:" + the_dir)
+        logging.info(ex+": complete getting tick")
